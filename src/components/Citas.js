dda43ee29a859f98474b2f7f377d56dc4f682e8b
Jorge Garcia <jorgealfonsogarcia@gmail.com>
2023-06-29 16:08:00 -0500

    :sparkles: (feat) Delete appointment.

--- src/components/Citas.js
+++ src/components/Citas.js
@@ -7,6 +7,7 @@ import { ReactComponent as XCircleFill } from 'bootstrap-icons/icons/x-circle-fi
 
 const Citas = () => {
     const [citas, setCitas] = useState([]);
+    const [citaToDelete, setCitaToDelete] = useState(null);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -24,12 +25,22 @@ const Citas = () => {
 
     const handleEdit = (id) => {
         console.log(`Editando cita con ID ${id}`);
-        // Navega a la página de edición de citas con el ID de la cita como parámetro
         navigate(`/editar/${id}`);
     };
 
-    const handleDelete = (id) => {
-        console.log(`Eliminando cita con ID ${id}`);
+    const handleDelete = async () => {
+        console.log(`Eliminando cita con ID ${citaToDelete}`);
+        try {
+            await axios.delete(`/citas/${citaToDelete}`);
+            setCitas(citas.filter(cita => cita.id !== citaToDelete));
+            setCitaToDelete(null);
+        } catch (error) {
+            console.error("Error deleting cita: ", error);
+        }
+    };
+
+    const openModal = (id) => {
+        setCitaToDelete(id);
     };
 
     return (
@@ -59,13 +70,30 @@ const Citas = () => {
                             <td>{cita.detalles}</td>
                             <td>{cita.estado}</td>
                             <td>
-                                <button type="button" className="btn btn-primary me-2" onClick={() => handleEdit(cita.id)}><PencilFill/></button>
-                                <button type="button" className="btn btn-danger" onClick={() => handleDelete(cita.id)}><XCircleFill/></button>
+                                <button type="button" className="btn btn-primary me-2" onClick={() => handleEdit(cita.id)}><PencilFill /></button>
+                                <button type="button" className="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteModal" onClick={() => openModal(cita.id)}><XCircleFill /></button>
                             </td>
                         </tr>
                     ))}
                 </tbody>
             </table>
+            <div className="modal" tabIndex="-1" id="deleteModal">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Confirmar eliminación</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <p>¿Estás seguro de que deseas eliminar esta cita?</p>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                            <button type="button" className="btn btn-danger" onClick={handleDelete} data-bs-dismiss="modal">Eliminar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
